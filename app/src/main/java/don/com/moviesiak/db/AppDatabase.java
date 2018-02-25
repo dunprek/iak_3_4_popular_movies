c0abefc97151b7b6ea1d43b5a1ac4379569b6b99
GIDEON\don <gideon.tobing@hotmail.com>
2018-02-25 11:27:56 +0700

    change inmemorydb builder to dbBuilder

--- app/src/main/java/don/com/moviesiak/db/AppDatabase.java
+++ app/src/main/java/don/com/moviesiak/db/AppDatabase.java
@@ -1,9 +1,15 @@
 package don.com.moviesiak.db;
 
+import android.arch.core.executor.DefaultTaskExecutor;
+import android.arch.lifecycle.MutableLiveData;
+import android.arch.persistence.db.SupportSQLiteDatabase;
 import android.arch.persistence.room.Database;
 import android.arch.persistence.room.Room;
 import android.arch.persistence.room.RoomDatabase;
 import android.content.Context;
+import android.support.annotation.NonNull;
+
+import java.util.List;
 
 /**
  * Created by don on 2/24/2018.
@@ -18,11 +24,16 @@ public abstract class AppDatabase extends RoomDatabase {
 
     public abstract FavoriteDao favoriteModel();
 
+    public static final String DATABASE_NAME = "basic-sample-db";
+
+    private final MutableLiveData<Boolean> mIsDatabaseCreated = new MutableLiveData<>();
+
 
     public static AppDatabase getInMemoryDatabase(Context context) {
         if (INSTANCE == null) {
             INSTANCE =
-                    Room.inMemoryDatabaseBuilder(context.getApplicationContext(), AppDatabase.class)
+//                    Room.inMemoryDatabaseBuilder(context.getApplicationContext(), AppDatabase.class)
+                    Room.databaseBuilder(context.getApplicationContext(), AppDatabase.class, DATABASE_NAME)
                             // To simplify the codelab, allow queries on the main thread.
                             // Don't do this on a real app! See PersistenceBasicSample for an example.
                             .allowMainThreadQueries()
@@ -30,6 +41,10 @@ public abstract class AppDatabase extends RoomDatabase {
         }
         return INSTANCE;
     }
+
+
+
+
 
     public static void destroyInstance() {
         INSTANCE = null;
